81a4c46830b643e3229b9db75d696e2c12d7fb8c
Serhii Soboliev <sergiy.sobolev@gmail.com>
2020-03-28 15:51:48 +0200

    cleanup

--- src/main/kotlin/org/dataart/pmsintegration/pmsclients/PmsClient.kt
+++ src/main/kotlin/org/dataart/pmsintegration/pmsclients/PmsClient.kt
@@ -3,6 +3,7 @@ package org.dataart.pmsintegration.pmsclients
 import org.dataart.pmsintegration.pmsclients.data.PracticeInfo
 import org.dataart.pmsintegration.pmsclients.data.PracticesInfo
 
+
 interface PmsClient {
     fun getAvailablePractices():PracticesInfo
     fun getAvailablePractice(practiceId: String): PracticeInfo
