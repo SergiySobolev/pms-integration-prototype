1ad9e6d0a3c6c102d3a57071241a71656e8f495d
Serhii Soboliev <sergiy.sobolev@gmail.com>
2020-04-05 23:53:19 +0300

    remove cloudbuild from install.sh

--- gcpprovision/install.sh
+++ gcpprovision/install.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 export PROJECT_ID=da-pms-integration-prototype
 export PROJECT_NAME=pms-integration-prototype
-export BACKEND_IMAGE=gcr.io/da-pms-integration-prototype/pms-integration-backend:1.0
-export FRONTEND_IMAGE=gcr.io/da-pms-integration-prototype/pms-integration-frontend:1.0
 
 gcloud projects create $PROJECT_ID --name=$PROJECT_NAME --set-as-default
 gcloud config set project $PROJECT_ID
@@ -10,8 +8,5 @@ gcloud config set project $PROJECT_ID
 gcloud services enable compute
 gcloud services enable deploymentmanager
 gcloud services enable containerregistry.googleapis.com
-gcloud services enable cloudbuild.googleapis.com
-
-gcloud compute instance-templates create-with-container backend-it --container-image $BACKEND_IMAGE
 
 gcloud deployment-manager deployments create backend-deployment --config dm/backend.yaml
